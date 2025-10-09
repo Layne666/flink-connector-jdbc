70f85a52e2f5ead088f7a7a34a28469923961ce4
Layne666 <791270330@qq.com>
2025-10-09 11:37:51 +0800

    feat(jdbc): Increase the maximum number of connections from 200 to 1024 to improve concurrent processing capabilities

--- flink-connector-jdbc-core/src/main/java/org/apache/flink/connector/jdbc/datasource/connections/SimpleJdbcConnectionProvider.java
+++ flink-connector-jdbc-core/src/main/java/org/apache/flink/connector/jdbc/datasource/connections/SimpleJdbcConnectionProvider.java
@@ -172,7 +172,7 @@ public class SimpleJdbcConnectionProvider implements JdbcConnectionProvider, Ser
         config.setPassword(password);
         config.setDriverClassName("com.mysql.cj.jdbc.Driver");
 
-        config.setMaximumPoolSize(200); // Maximum number of connections
+        config.setMaximumPoolSize(1024); // Maximum number of connections
         config.setMinimumIdle(20); // Minimal idle connection
         config.setIdleTimeout(
                 600_000); // Idle connection timeout, in milliseconds (10 minutes) - much less than
